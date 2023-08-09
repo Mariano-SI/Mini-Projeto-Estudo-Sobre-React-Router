b4802cffcb0c106a1b92eb7371ba3927cc9577aa
Mariano Silva <mariano.silva@dtidigital.com>
2023-08-08 23:45:49 -0300

    consumindo api

--- src/Components/Products/Products.tsx
+++ src/Components/Products/Products.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react'
 import { ProductsStyled } from './Products.style'
+import { Product } from '../../Types/Product';
 
-interface IProducts{}
 
-const Products = (props: IProducts) => {
-    console.log(props)
+
+const Products = () => {
+  const [products, setProducts] = useState<Array<Product>>([]);
+  
+
+  async function getProducts():Promise<void> {
+    const response = await fetch('https://ranekapi.origamid.dev/json/api/produto');
+    const json = await response.json()
+    setProducts(json);
+  }
+
+  useEffect( ()=>{
+    getProducts()
+  },[])
+  useEffect( ()=>{
+    console.log(products)
+  },[products])
+
   return (
     <ProductsStyled>
         <h1>Produtos</h1>
